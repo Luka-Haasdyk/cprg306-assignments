6a353c6fc33a422e7cfeb070ec8453cfdc007d7b
Luka-Haasdyk <Luka.Haasdyk@edu.sait.ca>
2024-10-27 18:34:51 -0600

    Update new-item.js

--- app/week-7/new-item.js
+++ app/week-7/new-item.js
@@ -61,6 +61,7 @@ export default function NewItemFunction({ onAddItem }) {
     "bg-blue-500 rounded-lg px-3 ml-1 mb-1 mt-1 w-9 h-3hover:bg-blue-600 active:bg-yellow-500 disabled:bg-gray-400 text-white font-bold";
   let inputStyle =
     "w-full p-2 bg-white text-black border border-gray-600 rounded-lg placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500";
+  let selectStyle =
     "p-2 bg-white text-black border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-[40%]";
   let submitBtnStyle =
     "w-full p-2 bg-blue-500 rounded-lg hover:bg-blue-700 active:bg-blue-800 text-white font-bold";
